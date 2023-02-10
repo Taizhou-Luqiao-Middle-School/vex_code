6a2bb5f95926da008cc0ebae37e34967c41bd212
L666no6 <72982866+L666no6@users.noreply.github.com>
2023-02-10 08:15:15 +0000

    done ( maybe )

--- testing2.cpp
+++ testing2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main ()
 {
-    cout << "fuck you ";
-    cout << "testing 1";
+    cout << "thank you ,I know how to use git now";
+    //cout << "testing 1";
     return 0;
 }
